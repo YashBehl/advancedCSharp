27d8ba7c54055bdac7197c14eca437837b2b68c5
YashBehl <147510285+YashBehl@users.noreply.github.com>
2023-11-24 16:19:42 +0530

    Add files via upload

---
+++ 23NovQues6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question6AsyncAwait
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            process1();
+            Console.ReadKey();
+        }
+        static async void process1()
+        {
+            Console.WriteLine("Process Started");
+
+            await Task.Delay(4000); // this holds execution for 4 seconds, so the next line of code will be executed after 4 seconds
+
+            Console.WriteLine("Process Completed");
+
+        }
+
+
+        /*Justification:
+        In this program, the main and process method are marked as async, 
+        so now our program has become of asynchronous programming type. return type 
+        of Main is Task. The main must be async in order to make child methods async
+        await Task.Delay(4000) holds the thread for 4 seconds
+        The async process1() method gets executed in a separate thread and the 
+        main application thread continues execution of the next statement 
+        and does not wait for the process1() to complete.
+        */
+    }
+}
---
+++ 23rdNovQues1and4.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment23rdNovember
+{
+    class Program
+    {
+        static int multiplicationOfTwoNumbers(int x, int y)
+        {
+            return x * y;
+        }
+
+        static void printName(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        static bool isHundred(int x)
+        {
+            return x == 100;
+        }
+        static void Main(string[] args)
+        {
+            // Q1. Try catch example
+            /*int div = 0;
+            
+            try
+            {
+                float res = 100 / div;
+                Console.WriteLine(res);
+            }
+            catch
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }*/
+
+
+
+            //Q4. Try to use Func, Action and Predicate any program. 
+            Func<int, int, int> mul = multiplicationOfTwoNumbers;
+
+            int result = mul(10, 5);
+
+            Console.WriteLine(result);
+
+
+
+            Action<string> printYourName  = printName;
+            printYourName("Yash");
+
+
+            Predicate<int> pred = isHundred;
+
+            bool output = pred(100);
+
+            Console.WriteLine(output);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
